c0787525de4a5ba7b4cab9c24a8be8f389e350ba
biv <nicholas.faiz@gmail.com>
2010-08-10 18:24:12 +1000

    better image of tommy, configuring static assets via rack

--- lib/tommy/engine.rb
+++ lib/tommy/engine.rb
@@ -4,5 +4,9 @@ require "rails"
 module Tommy
   class Engine < Rails::Engine
     engine_name :tommy
+    
+    initializer "static assets" do |app|
+      app.middleware.insert_after ::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public"
+    end
   end
 end
